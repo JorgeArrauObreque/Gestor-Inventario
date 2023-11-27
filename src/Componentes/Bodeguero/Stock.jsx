38ee4fcf60a2282835f68cd7229085b0ff2c12d1
jorge arrau <jorgearrau19@gmail.com>
2023-11-27 12:23:20 -0300

    correccion

--- src/Componentes/Bodeguero/Stock.jsx
+++ src/Componentes/Bodeguero/Stock.jsx
@@ -3,7 +3,32 @@ import { get_stock } from "../../Servicios/ProductoService";
 
 export default function Stock() {
     const [stock, setStock] = useState([]);
-
+    const handleDownloadExcel = async () => {
+        try {
+          // Código para descargar el archivo Excel
+          const response = await fetch('http://localhost:5136/api/Informe/export'); // Asegúrate de que la ruta sea correcta
+          const blob = await response.blob();
+    
+          const url = window.URL.createObjectURL(blob);
+          const a = document.createElement('a');
+          a.href = url;
+          const currentDate = new Date();
+          const formattedDate = currentDate.getFullYear() +
+            ('0' + (currentDate.getMonth() + 1)).slice(-2) +
+            ('0' + currentDate.getDate()).slice(-2) +
+            ('0' + currentDate.getHours()).slice(-2) +
+            ('0' + currentDate.getMinutes()).slice(-2) +
+            ('0' + currentDate.getSeconds()).slice(-2);
+    
+          a.download = 'Inventario_' + formattedDate + '.xlsx'; // Nombre del archivo con fecha
+          document.body.appendChild(a);
+          a.click();
+          document.body.removeChild(a);
+          window.URL.revokeObjectURL(url);
+        } catch (error) {
+          console.error(error);
+        }
+      }
     useEffect(() => {
         async function fetchStock() {
             try {
@@ -20,7 +45,14 @@ export default function Stock() {
     return (
         <>
         <div className="container">
-            <h1>Stock Pañol</h1>
+            <div className="">
+             
+                <h1>Stock Pañol</h1>
+            
+           
+                <button onClick={handleDownloadExcel} className="btn btn-success mt-2 mb-3">Exportar excel</button>
+               
+            </div>
             <table className="table">
                 
                 <tr className="table-head">
